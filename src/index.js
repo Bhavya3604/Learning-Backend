663ec554d359718434c6fb0ed13b1893d1e8cb4c
Bhavya3604 <bhavyashah220@gmail.com>
2025-11-11 23:22:48 +0530

    API response and error

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import connectDB from "./db/index.js"
 dotenv.config()
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    })
+})
+.catch((error) => {
+    console.log("Error: ", error)
+})
 
 
 
